2342be88bdc2343af18e679bb1fb70aad32ddc61
PriyanshBordia <priyanshbordia1@gmail.com>
2021-05-13 18:24:28 +0530

    setup0

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', default=True)
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = [env('ALLOWED_HOSTS'), '127.0.0.1']
 
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    
 ]
 
 MIDDLEWARE = [
