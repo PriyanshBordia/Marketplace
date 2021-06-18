372fbc597f17618c90797593fe1ce58a4d5a5ea2
PriyanshBordia <priyanshbordia1@gmail.com>
2021-06-19 00:06:51 +0530

    VENV TEST0

--- circle/models.py
+++ circle/models.py
@@ -56,14 +56,14 @@ class Person(models.Model):
         return f"{self.first} {self.last}  {self.age}  {self.sex}"
 
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
 
 
 class Message(models.Model):
