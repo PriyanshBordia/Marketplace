a929cef5ae0668ce5db37a8371b037288a1029aa
PriyanshBordia <priyanshbordia1@gmail.com>
2021-06-19 13:33:16 +0530

    settings updated

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', 
+	'django_extensions',
     'accounts',
 ]
 
