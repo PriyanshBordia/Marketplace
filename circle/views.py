8a5d0054f2b675e08ea2aa8d6b9f20375b81fdea
PriyanshBordia <priyanshbordia1@gmail.com>
2021-06-23 22:01:33 +0530

    gunicorn added

--- circle/views.py
+++ circle/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render
 from django.urls import reverse
+from django.shortcuts import render
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from .models import Article
 
