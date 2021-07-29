61da4a59486e0afdc2bf03a5723457383c8310a6
PriyanshBordia <priyanshbordia1@gmail.com>
2021-07-29 12:28:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+git checkout .
 git pull
 source venv/bin/activate
 pip install -r requirements.txt
+python3 manage.py collectstatic
 python3 manage.py test circle
+python3 manage.py makemigrations
 python3 manage.py migrate
+git add .
+git commit -am "deploy"
+git push
+sleep()
+git push heroku main
