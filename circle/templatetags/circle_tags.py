75dfe7350b5f55b5e6bf5e528d2ea7d632053217
PriyanshBordia <priyanshbordia1@gmail.com>
2021-08-02 01:06:44 +0530

    bug fixes ang beautify

--- circle/templatetags/circle_tags.py
+++ circle/templatetags/circle_tags.py
@@ -1,22 +1,25 @@
 from django import template
 
-from termcolor import cprint
+from django.db.models import Q
 
 from circle.models import Article, Person, Tag, Message, Chat
 
+from termcolor import cprint
+
+
 register = template.Library()
 
-
-# @register.filter(name='isSender')
-# def isSender(value, arg):
-# 	try:
-# 		person = Person.objects.get(pk=person_id)
-# 		chat_id = arg[0]
-# 		chat = Chat.objects.get(pk=chat_id)
-# 	except Chat.DoesNotExist:
-# 		cprint("Chat does not exist", 'red')
-# 		return False
-
+@register.filter(name='get_chat_id')
+def chatId(value, arg):
+	try:
+		person_id = int(arg)
+		person = Person.objects.get(pk=person_id)
+		friend = value
+		return Chat.objects.filter(Q(left=person, right=friend) | Q(left=friend, right=person)).first().id
+	except Person.DoesNotExist:
+		cprint("Person does not exist", 'red')
+	except Chat.DoesNotExist:
+		cprint("Chat does not exist", 'red')
 
 @register.filter(name='isFriend')
 def isFriend(value, arg):
