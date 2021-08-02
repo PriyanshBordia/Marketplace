586938935d90aae2a29688c1b502e96e990e36e7
PriyanshBordia <priyanshbordia1@gmail.com>
2021-08-02 22:11:57 +0530

    removed unused packages

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ pip install --upgrade pip
 pip install -r requirements.txt
 # python3 manage.py collectstatic
 # echo "yes"
-# python3 manage.py test circle
+python3 manage.py test circle
 # if [python3 manage.py test circle] then
-python3 manage.py makemigrations
+# python3 manage.py makemigrations
 python3 manage.py migrate
 git add .
 git commit -am "deploy"
