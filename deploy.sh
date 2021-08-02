09550de3dbc92a010ecc759b764e8ae4dde2a2ab
PriyanshBordia <priyanshbordia1@gmail.com>
2021-08-02 18:29:02 +0530

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 git pull
 source venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 # python3 manage.py collectstatic
 # echo "yes"
-if python3 manage.py test circle
+# python3 manage.py test circle
+# if [python3 manage.py test circle] then
 python3 manage.py makemigrations
 python3 manage.py migrate
 git add .
@@ -12,3 +14,4 @@ git commit -am "deploy"
 git push
 # sleep 3
 git push heroku main
+# fi 
