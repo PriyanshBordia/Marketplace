fabb901b844705bf2d11aa5d75727f257de128e7
PriyanshBordia <priyanshbordia1@gmail.com>
2021-08-08 00:05:06 +0530

    Update utils.py

--- circle/utils.py
+++ circle/utils.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from termcolor import cprint
 
 def set_unique_name(image, pub_ts):
-	image_name = 'Image_' + pub_ts.strftime("%Y-%m-%d_at_%H.%M.%S")
-	image = '../staticfiles/' + str(image)
-	cprint(image, 'cyan')
-	os.rename(image, 'static/media/images/' + str(image_name) + '.png')
+	img_dir = 'static/media/images/persons/'
+	image_name = 'Image_' + pub_ts.strftime("%Y-%m-%d_at_%H.%M.%S") + '.png'
+	image = img_dir + str(image)
+	os.rename(img_dir + image,  str(image_name))
